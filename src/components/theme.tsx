b9798a3bac7ae51829a8af529dda2d22239e112b
youssefsaki <igtbrasaic@gmail.com>
2024-03-22 02:36:51 +0000

    just about responsive

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -13,7 +13,7 @@ export const Theme = () => {
     <motion.button 
         className='fixed bottom-5 border border-white right-5 h-[48px] w-[48px] bg-white bg-opacity-80 flex justify-center *:
         items-center rounded-full shadow-2xl hover:scale-110 transition-all active:scale-105 dark:bg-gray-950 dark:text-gray-300
-        outline-none
+        outline-none max-sm:w-[38px] max-sm:h-[38px]
     '
         initial={{x: 40, opacity: 0}}
         animate={{x: 0, opacity: 1}}
